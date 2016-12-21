47fc54d84e62e38946bdb3a93f2065ddc059f9e7
gavinzhou <gavin.zhou@gmail.com>
2016-12-21 11:19:51 +0900

    remove cache

--- main.go
+++ main.go
@@ -3,9 +3,5 @@ package main
 import "net/http"
 
 func init() {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		http.FileServer(http.Dir("public"))
-		w.Header().Set("Cache-Control", "public, max-age=7200")
-    w.Header().Set("Pragma", "Public")
-	})
+	http.Handle("/", http.FileServer(http.Dir("public")))
 }
