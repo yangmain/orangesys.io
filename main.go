8ba169b85b87a7962c0ffbf04f8bbcef8d7c46a0
gavinzhou <gavin.zhou@gmail.com>
2016-12-21 10:45:09 +0900

    add edge cache

--- main.go
+++ main.go
@@ -3,5 +3,9 @@ package main
 import "net/http"
 
 func init() {
-	http.Handle("/", http.FileServer(http.Dir("public")))
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		http.FileServer(http.Dir("public"))
+		w.Header().Set("Cache-Control", "public, max-age=7200")
+    w.Header().Set("Pragma", "Public")
+	})
 }
