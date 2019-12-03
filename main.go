396a1b6ea73a18cd6f31e11abe62ae379d80d673
gavinzhou <gavin.zhou@gmail.com>
2019-12-03 10:41:42 +0900

    test deploy with ci

--- main.go
+++ main.go
@@ -1,7 +1,22 @@
 package main
 
-import "net/http"
+import (
+	"log"
+	"net/http"
+	"os"
+)
 
-func init() {
+func main() {
 	http.Handle("/", http.FileServer(http.Dir("public")))
+
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+		log.Printf("Defaulting to port %s", port)
+	}
+
+	log.Printf("Listening on port %s", port)
+	if err := http.ListenAndServe(":"+port, nil); err != nil {
+		log.Fatal(err)
+	}
 }
